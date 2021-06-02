2f7a998a41e974a04a301b36c142d4e4286e498d
Paul Coroneos <paul@pcoroneos.com>
2021-06-02 00:02:18 -0500

    complete iterative preorder method

--- binary-tree/index.test.ts
+++ binary-tree/index.test.ts
@@ -25,4 +25,28 @@ describe("BinaryTree()",() => {
         binaryTree.insert(7);
         expect(binaryTree.root?.right?.right?.left).toMatchObject({data:7,left:null,right:null});
     });
+    it("handles preorder traversal iteratively", () => {
+        const binaryTree = new BinaryTree();
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([]);
+        binaryTree.insert(5);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5]);
+        binaryTree.insert(1);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5,1]);
+        binaryTree.insert(4);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5,1,4]);
+        binaryTree.insert(8);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5,1,4,8]);
+    });
+    it.skip("handles preorder traversal recursively", () => {
+        const binaryTree = new BinaryTree();
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([]);
+        binaryTree.insert(5);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5]);
+        binaryTree.insert(1);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5,1]);
+        binaryTree.insert(4);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5,1,4]);
+        binaryTree.insert(8);
+        expect(binaryTree.preorderTraversal(binaryTree.root,"iterative")).toStrictEqual([5,1,4,8]);
+    });
 });
--- binary-tree/index.ts
+++ binary-tree/index.ts
@@ -1,33 +1,57 @@
 import { Node } from "../node";
 
 export class BinaryTree {
-  root: Node | null;
+    root: Node | null;
 
-  constructor(root:Node|null = null) {
-      this.root = root;
-  }
+    constructor(root: Node | null = null) {
+        this.root = root;
+    }
 
-  public insert(val:number) {
+    public insert(val: number) {
+        const insertFn = (node: Node | null, val: number) => {
+            //Scenario 1: Base case root is null
+            if (node == null) {
+                node = new Node(val);
+            }
 
-      const insertFn = (node:Node | null,val:number) => {
-      //Scenario 1: Base case root is null
-          if(node == null) {
-              node = new Node(val);
-          }
+            //Scenario 2: value is greater than right node
+            else if (val > node?.data) {
+                node.right = insertFn(node?.right, val);
+            }
+            //scenario 3: value is less than right node
+            else {
+                node.left = insertFn(node?.left, val);
+            }
 
-          //Scenario 2: value is greater than right node
-          else if(val > node?.data ) {
-              node.right = insertFn(node?.right,val);
-          }
-          //scenario 3: value is less than right node
-          else {
-              node.left = insertFn(node?.left,val);
-          }
+            return node;
+        };
 
-          return node;
-      };
+        this.root = insertFn(this.root, val);
+    }
 
-      this.root = insertFn(this.root,val);
+    public preorderTraversal(node: Node | null, type: "recursive" | "iterative") {
+        const arr: Node["data"][] = [];
 
-  }
+        if (node == null) {
+            return arr;
+        }
+
+        if (type === "iterative") {
+            const stack: Node[] = [];
+            stack.push(node);
+
+            while (stack.length) {
+                const node = stack.pop() as Node;
+                arr.push(node.data);
+                if (node?.right) stack.push(node.right);
+                if (node?.left) stack.push(node.left);
+            }
+        }
+
+        else {
+            //TODO Recursive
+        }
+
+        return arr;
+    }
 }
